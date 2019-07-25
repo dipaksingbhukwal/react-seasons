a73fdf780305b2a1da6abe68a4cdb79802c94fce
dipaksingbhukwal <35825317+dipaksingbhukwal@users.noreply.github.com>
2019-07-25 16:24:44 -0700

    Added styling and spinner component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,12 +1,34 @@
 import React from "react";
+import "./SeasonDisplay.css";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it is chilly!",
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? "summer" : "winter";
+  } else {
+    return latitude > 0 ? "winter" : "summer";
+  }
+};
 
 const SeasonDisplay = props => {
-  console.log(props.p);
-  let a = [<li>abc</li>, <li>abc</li>, <li>abc</li>];
-  let b = a.map(ac => ac);
+  const season = getSeason(props.latitude, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
   return (
-    <div>
-      <ul>{b}</ul>Season Display
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} massive icon icon-left`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} massive icon icon-right`} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { latitude: null };
+  state = { latitude: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ latitude: position.coords.latitude }),
-      err => console.log(err)
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
   render() {
+    if (this.state.errorMessage && !this.state.latitude)
+      return <div>Error : {this.state.errorMessage}</div>;
+    if (!this.state.errorMessage && this.state.latitude)
+      return <SeasonDisplay latitude={this.state.latitude} />;
     return (
       <div>
-        Lattitude {this.state.latitude}
-        {true && <SeasonDisplay p="null" b />}
+        <Spinner />
       </div>
     );
   }
